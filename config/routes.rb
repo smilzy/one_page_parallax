93c16105507d27dde0674975f0cbce25ae631033
Aleksander Wozniak <4wozniak@gmail.com>
2017-06-23 20:34:02 +0000

    Added mailer...

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
     root to: 'pages#home'
+    get     'contact',  to: 'contacts#new', as: 'new_contact'
+    resources :contacts,              only: :create
+
 end
